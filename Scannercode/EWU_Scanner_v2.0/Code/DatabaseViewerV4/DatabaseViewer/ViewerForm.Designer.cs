a813eca71789145057c650898686fad5a5dbf81b
sam <sitienei85@gmail.com>
2012-04-28 21:29:14 -0700

    The bugs had been fixed, counting patterns is good on 126-45-4515 and 126454515 with exceptional of these pattern format aa126-45-4515vv  and aa126454515vv that will be fixed in the coming days. By Sam and Sijie

---
+++ Scannercode/EWU_Scanner_v2.0/Code/DatabaseViewerV4/DatabaseViewer/ViewerForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace DatabaseViewer
+{
+    partial class ViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.theTabPanel = new System.Windows.Forms.TabControl();
+            this.SocialFound = new System.Windows.Forms.TabPage();
+            this.dataGridView_Social = new System.Windows.Forms.DataGridView();
+            this.CreditFound = new System.Windows.Forms.TabPage();
+            this.dataGridView_Credit = new System.Windows.Forms.DataGridView();
+            this.NotScanned = new System.Windows.Forms.TabPage();
+            this.dataGridView_NotScanned = new System.Windows.Forms.DataGridView();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openDBFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.theTabPanel.SuspendLayout();
+            this.SocialFound.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Social)).BeginInit();
+            this.CreditFound.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Credit)).BeginInit();
+            this.NotScanned.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_NotScanned)).BeginInit();
+            this.mainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.theTabPanel);
+            this.panel1.Location = new System.Drawing.Point(-2, 148);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1021, 589);
+            this.panel1.TabIndex = 0;
+            // 
+            // theTabPanel
+            // 
+            this.theTabPanel.Controls.Add(this.SocialFound);
+            this.theTabPanel.Controls.Add(this.CreditFound);
+            this.theTabPanel.Controls.Add(this.NotScanned);
+            this.theTabPanel.Location = new System.Drawing.Point(14, 14);
+            this.theTabPanel.Name = "theTabPanel";
+            this.theTabPanel.SelectedIndex = 0;
+            this.theTabPanel.Size = new System.Drawing.Size(992, 560);
+            this.theTabPanel.TabIndex = 0;
+            // 
+            // SocialFound
+            // 
+            this.SocialFound.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SocialFound.Controls.Add(this.dataGridView_Social);
+            this.SocialFound.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SocialFound.Location = new System.Drawing.Point(4, 22);
+            this.SocialFound.Name = "SocialFound";
+            this.SocialFound.Padding = new System.Windows.Forms.Padding(3);
+            this.SocialFound.Size = new System.Drawing.Size(984, 534);
+            this.SocialFound.TabIndex = 0;
+            this.SocialFound.Text = "Social Security Numbers Found";
+            // 
+            // dataGridView_Social
+            // 
+            this.dataGridView_Social.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_Social.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView_Social.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Social.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView_Social.Name = "dataGridView_Social";
+            this.dataGridView_Social.Size = new System.Drawing.Size(988, 535);
+            this.dataGridView_Social.TabIndex = 0;
+            // 
+            // CreditFound
+            // 
+            this.CreditFound.Controls.Add(this.dataGridView_Credit);
+            this.CreditFound.Location = new System.Drawing.Point(4, 22);
+            this.CreditFound.Name = "CreditFound";
+            this.CreditFound.Size = new System.Drawing.Size(984, 534);
+            this.CreditFound.TabIndex = 2;
+            this.CreditFound.Text = "Credit Card Numbers Found";
+            this.CreditFound.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView_Credit
+            // 
+            this.dataGridView_Credit.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView_Credit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Credit.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.dataGridView_Credit.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView_Credit.Name = "dataGridView_Credit";
+            this.dataGridView_Credit.Size = new System.Drawing.Size(988, 535);
+            this.dataGridView_Credit.TabIndex = 0;
+            // 
+            // NotScanned
+            // 
+            this.NotScanned.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NotScanned.Controls.Add(this.dataGridView_NotScanned);
+            this.NotScanned.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.NotScanned.Location = new System.Drawing.Point(4, 22);
+            this.NotScanned.Name = "NotScanned";
+            this.NotScanned.Padding = new System.Windows.Forms.Padding(3);
+            this.NotScanned.Size = new System.Drawing.Size(984, 534);
+            this.NotScanned.TabIndex = 1;
+            this.NotScanned.Text = "Files Not Scanned";
+            // 
+            // dataGridView_NotScanned
+            // 
+            this.dataGridView_NotScanned.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView_NotScanned.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView_NotScanned.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_NotScanned.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView_NotScanned.Name = "dataGridView_NotScanned";
+            this.dataGridView_NotScanned.Size = new System.Drawing.Size(988, 535);
+            this.dataGridView_NotScanned.TabIndex = 0;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(1016, 24);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.loadToolStripMenuItem.Text = "Load...";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DatabaseViewer.Properties.Resources.longLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(793, 107);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openDBFileDialog
+            // 
+            this.openDBFileDialog.Filter = "Scanner Database File|*.sdb";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.ForeColor = System.Drawing.Color.Maroon;
+            this.lblDescription.Location = new System.Drawing.Point(847, 127);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(157, 13);
+            this.lblDescription.TabIndex = 3;
+            this.lblDescription.Text = "EWUScanner Database Viewer";
+            // 
+            // ViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1016, 734);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mainMenu);
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "ViewerForm";
+            this.Text = "EWUScanner Database Viewer";
+            this.panel1.ResumeLayout(false);
+            this.theTabPanel.ResumeLayout(false);
+            this.SocialFound.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Social)).EndInit();
+            this.CreditFound.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Credit)).EndInit();
+            this.NotScanned.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_NotScanned)).EndInit();
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl theTabPanel;
+        private System.Windows.Forms.TabPage SocialFound;
+        private System.Windows.Forms.TabPage NotScanned;
+        private System.Windows.Forms.DataGridView dataGridView_Social;
+        private System.Windows.Forms.DataGridView dataGridView_NotScanned;
+        private System.Windows.Forms.OpenFileDialog openDBFileDialog;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TabPage CreditFound;
+        private System.Windows.Forms.DataGridView dataGridView_Credit;
+    }
+}
+
---
+++ Scannercode/EWU_Scanner_v2.0/Code/SSNScannerDLL/SSNScannerDLL/Scanner.cpp
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <regex>
+
+#define DLL_EXPORT __declspec(dllexport)
+
+using namespace std;
+
+namespace SSNValidation
+{
+	int AreaCheckPre(int area);
+	int AreaCheckPost(int area);
+	bool isAd(string s);
+	int KeyphraseCheck(const char s[]);
+	enum Pattern {
+		d9 = 0,
+		d324
+	};
+}
+namespace CCNValidation
+{
+	int LuhnCheck(string input);
+	int KeyphraseCheck(const char s[]);
+	enum Pattern {
+		Visa = 0,
+		MC,
+		Amex,
+		Discover,
+		Diners,
+		JCB
+	};
+}
+
+void strip(string *str);
+enum {
+    NoNumFound = 0,
+    Low,
+    Medium,
+    High
+};
+
+extern "C"
+{
+	/*
+	 * Scan for social security numbers
+	 *
+	 * s: the character array to be scanned, usually the contents of a file
+	 * length: the length of the array
+	 * count: the address of the integer variable to be set to the number of ssns found in s
+	 * patternCount: the count of each pattern, must be of size 2
+	 *		patternCount[0] is for 9 digit patterns
+	 *		patternCount[1] is for the 3-2-4 pattern
+	 */
+	DLL_EXPORT int ScanSSN(const char s[], int length, int *count, int *patternCount[])
+	{
+		int priority = NoNumFound;
+		int type; // for regex
+		*count = 0;
+		*patternCount[0] = 0;
+		*patternCount[1] = 0;
+		int areaNum; // first three digits of a SSN
+		int tempPriority; // to maintain max priority throught the file
+		string NineDigit; // this holds each token in the file that matches the regex
+		regex rgx("(\\b\\d{3}-\\d{2}-\\d{4}\\b)|(\\b\\d{9}\\b)"); 
+
+		// tokenizes the input string and checks each token for the regex pattern, we can then iterate though the matches
+		cregex_token_iterator next(s, s+length-1, rgx); 
+		cregex_token_iterator end;
+
+		for(;next != end; next++)
+		{
+			tempPriority = priority;
+			priority = 0;
+			NineDigit = next->str();
+			if(NineDigit[3] == '-')
+				patternCount[1]++;
+			else
+				patternCount[0]++;
+			strip(&NineDigit); // takes out every nondigit from the string
+
+			areaNum = atoi(NineDigit.substr(0, 3).c_str());
+			priority += SSNValidation::AreaCheckPost(areaNum);
+			priority += SSNValidation::AreaCheckPre(areaNum);
+			if(SSNValidation::isAd(NineDigit) || NineDigit.substr(3, 2).compare("00") == 0 || NineDigit.substr(5, 4).compare("0000") == 0) 
+				priority = NoNumFound;
+
+			if(priority) //priority != 0
+				++(*count);
+			if(priority < tempPriority)
+				priority = tempPriority;
+		}
+
+		if(priority) //priority != 0
+			priority += SSNValidation::KeyphraseCheck(s); // check for keyphrases
+		return priority;
+	}
+
+	/*
+	 * Scan for Credit Card Numbers
+	 *
+	 * s: the character array to be scanned, usually the contents of a file
+	 * length: the length of the array
+	 * count: the address of the integer variable to be set to the number found in s
+	 * patternCount: used to count instances of specific patters, must be size 6
+	 *		patternCount[0] is for Visa
+	 *		patternCount[1] is for Master Card
+	 *		patternCount[2] is for American Express
+	 *		patternCount[3] is for Discover
+	 *		patternCount[4] is for Diners Club
+	 *		patternCount[5] is for JCB
+	 */
+	DLL_EXPORT int ScanCCN(char s[], int length, int *count, int *patternCount[])
+	{
+		int priority = NoNumFound;
+		*count = 0;
+		*patternCount[0] = 0;
+		*patternCount[1] = 0;
+		*patternCount[2] = 0;
+		*patternCount[3] = 0;
+		*patternCount[4] = 0;
+		*patternCount[5] = 0;
+		int tempPriority; // to maintain max priority throught the file
+		string current;
+		regex rgx("(\\b(4[0-9]{12}([0-9]{3})?|5[1-5][0-9]{14}|6(011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(0[0-5]|[68][0-9])[0-9]{11}|(2131|1800|35[0-9]{3})[0-9]{11})\\b)"); 
+		// tokenizes the input string and checks each token for the regex pattern, we can then iterate though the matches
+		cregex_token_iterator next(s, s+length-1, rgx);
+		cregex_token_iterator end;
+
+		for(;next != end; next++)
+		{
+			tempPriority = priority;
+			current = next->str();
+
+			priority += CCNValidation::LuhnCheck(current);
+
+			if(priority) //priority != 0
+				++(*count);
+			if(priority < tempPriority)
+				priority = tempPriority;
+		}
+
+		if(priority) //priority != 0
+			priority += CCNValidation::KeyphraseCheck(s); // check for keyphrases
+
+		return priority;
+	}
+}
+
+/*
+* check if area is in valid ranges for pre 6/25/2011 
+*/
+int SSNValidation::AreaCheckPre(int area) 
+{
+	if(area == 0) {
+		return NoNumFound;
+	}
+	if (area == 666)
+		return NoNumFound;
+	if(area < 734) {
+		return Low;
+	}
+	if(area < 750) {
+		return NoNumFound;
+	}
+	if(area < 773) {
+		return Low;
+	}
+	return NoNumFound;
+}
+/*
+* check if area is in valid ranges for post 6/25/2011 
+*/
+int SSNValidation::AreaCheckPost(int area)
+{
+	if(area == 0)
+		return NoNumFound;
+	if(area == 666)
+		return NoNumFound;
+	if(area < 900)
+		return Low;
+	return NoNumFound;
+}
+/*
+* check if this ssn is known to have been used in Ads
+*/
+bool SSNValidation::isAd(string s)
+{
+	if(s.compare("078051120") == 0)
+		return true;
+	if(s.compare("457555462") == 0)
+		return true;
+	if(s.substr(0,8).compare("98765432") == 0)
+		return true;
+	return false;
+}
+/*
+* search for keyphrases for context based priority  
+*/
+int SSNValidation::KeyphraseCheck(const char s[])
+{
+	// Search for keyphrases, if one found return Low, else NoNumFound
+	regex rgx("(\\bsocial security number)|(\\bssn)|(\\bstudent id)", regex_constants::icase);
+	if(regex_search(s, rgx))
+		return Low;
+
+	return NoNumFound;
+}
+//---------------------END SSNValidation Functions-------------------------------------------------
+
+//---------------------Start CCNValidation Functions------------------------------------------------
+int CCNValidation::LuhnCheck(string input)
+{
+	int sum = 0;
+	int digit = 0;
+	int addend = 0;
+	bool timesTwo = false;
+
+	for (int i = input.length() - 1; i >= 0; i--) 
+	{
+		digit = atoi(&input[i]);
+		if (timesTwo) 
+		{
+			addend = digit * 2;
+			if (addend > 9) 
+			{
+				addend -= 9;
+			}
+		}
+		else 
+		{
+			addend = digit;
+		}
+		sum += addend;
+		timesTwo = !timesTwo;
+	}
+
+	int modulus = sum % 10;
+	if(modulus == 0)
+		return Low;
+	return NoNumFound;
+}
+
+int CCNValidation::KeyphraseCheck(const char s[])
+{
+	// Search for keyphrases, if one found return Low, else NoNumFound
+	regex rgx("(\\bcredit card( number)?)|(\\bccn)", regex_constants::icase);
+	if(regex_search(s, rgx))
+		return Low;
+
+	return NoNumFound;
+}
+//---------------------END CCNValidation Functions------------------------------------------------
+
+// remove any nondigit character from str
+void strip(string *str)
+{
+	for( int i = str->length() - 1; i >= 0; --i)
+	{
+		if(!isdigit(str->at(i)))
+			str->erase(i,1);
+	}
+}
+
+// For Debugging
+int main(void)
+{
+	ifstream fin("testinput.txt");
+	int length, count;
+	fin.seekg(0, std::ios::end);    // go to the end
+	length = fin.tellg();           // report location (this is the lenght)
+	fin.seekg(0, std::ios::beg);
+	char *str = new char[length];
+	fin.read(str, length);       // read the whole file into the buffer
+	fin.close();
+
+	int testPri = ScanSSN(str, length, &count);
+	cout << "returned priority: " << testPri << endl;
+	cout << "returned count: " << count << endl;
+	cin >> *str;
+}
